ef777b080b7a02f583c5752bb6564df211afe307
Salman Ahsan <salmanahsan@iut-dhaka.edu>
2022-10-19 10:47:32 +0600

    merge

--- index.js
+++ index.js
@@ -13,53 +13,6 @@ app.get('api/homepage', (req, res)=>{
     res.json("Welcome to the homepage of MeSeeks")
 })
 
-// app.post('/api/register', async (req,res)=>{
-//     try{
-//         const user = await User.create(
-//             {
-//                 firstname: req.body.firstname,
-//                 lastname: req.body.lastname,
-//                 email: req.body.email,
-//                 password: req.body.password,
-//                 confirmPassword: req.body.confirmPassword,
-//                 checkbox: req.body.checkbox
-//             }
-//         )
-//         res.json({status: "ok"})
-//     }
-//     catch(err)
-//     {
-//         console.log(err)
-//         console.log({status: 'error', error: "Duplicate Email"})
-//     }
-//     res.json({status:'ok'})    
-//     console.log(req.body)
-// })
-
-// app.post('/api/login', async (req,res)=>{
-//     const user = await User.findOne( {
-//         email: req.body.email,
-//         password: req.body.password,
-//     })
-    
-//     console.log(user)
-
-//     if(user){
-
-//         const token = jwt.sign(
-//             {
-//                 name: user.firstname,
-//                 email: user.email
-//             },
-//             process.env.SECRET
-//         )
-//         return res.json({status: 'ok', user: true})
-//     }
-//     else{
-//         return res.json({status: 'Unidentified Personnel', user: false})
-//     }
-// })
-
 const userRoutes = require('./routes/userRoutes');
 app.use("/api", userRoutes); 
 
